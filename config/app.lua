651a3092c820b60e694bfe9a63f6b74b3d18d239
Athos Benther <athum@live.com>
2023-10-16 19:30:03 -0300

    engine updates

--- config/app.lua
+++ config/app.lua
@@ -2,6 +2,7 @@ return {
     app = {
         env = env("APP_ENV","production"),
         debug = env("APP_DEBUG", false),
-        name = env("APP_NAME", "CP77 CET Mod")
+        name = env("APP_NAME", "Cyberpunk 2077 Cyber Eengine Tweaks Mod"),
+        shortName = env("APP_SHORT_NAME", "CP77 CET Mod"),
     }
 }
